168886f8575278d0a1081a55eaca21217dfbe4fc
evstrife <wus217@wfu.edu>
2019-10-24 03:58:22 -0400

    card complete

--- Deck.h
+++ Deck.h
@@ -4,6 +4,17 @@
 #include "Card.h"
 
 class Deck {
+public:
+    Deck(); //default constructor
+    Deck (const Deck& C );//copy constructor
+    Deck& operator= (const Deck& rhs );//copy assignment operator
+    ~Deck();//destructor
+    void populateDeck();//populates the deck
+    void shuffleCard(); //shuffles the cards
+    Card dealCard();//deals a card
+    bool addCard(Card v);//adds a card to the discard cards
+    void discardPrint();//prints out the discard cards
+
 
 private:
     Card* cards;
